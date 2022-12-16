c5156d47e313cf2fe17899f176bfc5ff34749eab
MatvienokVS <Skavaava@gmail.com>
2022-12-16 22:53:29 +0300

    Поиск решения задачи 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+using static System.Console;
+Clear();
+
+int n = Convert.ToInt32(ReadLine()!);
+
+int result = Convert.ToInt32(1);
+
+while (result <= n)
+{
+	if (result % 2 == 0)
+	{
+		Write($"{result} ");
+		result += 1;
+	}
+}
