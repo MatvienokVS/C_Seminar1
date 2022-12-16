e3a6cf3e95c0c619029b195ee2066b60dca05358
MatvienokVS <Skavaava@gmail.com>
2022-12-16 19:36:55 +0300

    Выполнения задания 2, 4, 6

---
+++ Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+using static System.Console;
+Clear();
+
+int a = Convert.ToInt32(ReadLine());
+int b = Convert.ToInt32(ReadLine());
+
+if (a > b)
+{
+	WriteLine($"Большее число {a}");
+}
+else
+{
+	WriteLine($"Большее число {b}");
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using static System.Console;
+Clear();
+
+int a = Convert.ToInt32(ReadLine()!);
+int b = Convert.ToInt32(ReadLine()!);
+int c = Convert.ToInt32(ReadLine()!);
+
+int max = a;
+
+if (max < b)
+{
+	max = b;
+	if (max < c)
+	{
+		max = c;
+	}
+}
+WriteLine($"Максимальное число {max}");
